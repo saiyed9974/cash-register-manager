d4eda8cd0185a33a6abd9ed49b84f647eb27de59
saiyed9974 <saiyed9974@gmail.com>
2023-11-28 17:51:06 +0530

    done correction in condition and code

--- app.js
+++ app.js
@@ -3,21 +3,32 @@ const cashReturn = document.querySelector("#cashReturn");
 const checkbtn = document.querySelector("#checkbtn");
 const errormsg = document.querySelector("#errormsg");
 
-const Notes = [2000,500,200,100,50,20,10,5,1];
+const Notes = ["2000","500","200","100","50","20","10","5","1"];
 
 
 
 checkbtn.addEventListener("click", function clickHandler(){
 
-    var totalBill = billAmount.value;
-    var givenamount = cashReturn.value;
-  
-   if(billAmount.value > 0 ){
-
+    var totalBill = parseInt(billAmount.value);
+    var givenamount = parseInt(cashReturn.value) ;
    
-            if(cashReturn.value > billAmount.value){
+  
+   if(totalBill > 0 ){
 
-                
+            errormsg.style.display = "none";
+    
+   
+            if(givenamount >= totalBill){
+
+                console.log(givenamount >= totalBill);
+                               
+                console.log("cashReturn="+givenamount);
+                console.log("billamount ="+totalBill);
+               
+                for(let i = 1;i < Notes.length;i++){
+
+                    console.log(Notes[i]);
+                }
 
                 
 
@@ -39,6 +50,6 @@ checkbtn.addEventListener("click", function clickHandler(){
 
 
 function Showmsg(err) {
-
+    errormsg.style.display = "block";
     errormsg.innerText = err;
 }
